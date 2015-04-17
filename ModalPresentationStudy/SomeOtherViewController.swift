821f2c5037f82e15500858bd24fc0f80d8d26032
Daniel R <danramteke@gmail.com>
2015-04-16 20:11:41 -0400

    it works

---
+++ ModalPresentationStudy/SomeOtherViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SomeOtherViewController.swift
+//  ModalPresentationStudy
+//
+//  Created by Daniel Ramteke on 4/16/15.
+//  Copyright (c) 2015 Dan Ramteke Consulting, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SomeOtherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.modalPresentationStyle = .Custom
+    }
+
+
+    @IBAction func close() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+//
+
+}
